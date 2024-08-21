ff26aacc065730d9d4ef8931ac66429513fe8cda
dio <dioputrautama@gmail.com>
2024-08-21 15:33:15 +0700

    Add MainTabView

--- RickAndMorty/Modules/MainTabView/MainTabView.swift
+++ RickAndMorty/Modules/MainTabView/MainTabView.swift
@@ -6,10 +6,43 @@
 //
 
 import SwiftUI
+import Combine
+
+class TabBarVisibility: ObservableObject {
+    static let shared = TabBarVisibility()
+    @Published var isHideTabBar: Bool = false
+}
 
 struct MainTabView: View {
+    @State private var selectedTab: TabType = .home
+    @StateObject private var tabBarVisibility = TabBarVisibility.shared
+
+    init() {
+        UITabBar.appearance().isHidden = true
+    }
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            TabView(selection: $selectedTab) {
+                switch selectedTab {
+                case .home:
+                    HomeView()
+                case .explore:
+                    ExploreView()
+                case .puzzle:
+                    HomeView()
+                }
+            }
+
+            if !tabBarVisibility.isHideTabBar {
+                VStack() {
+                    Spacer()
+                    CustomTabBarView(selectedTab: $selectedTab)
+                        .padding(.horizontal, 32)
+                }
+                .zIndex(1)
+            }
+        }
     }
 }
 
